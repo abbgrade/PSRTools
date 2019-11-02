fde862b06498d92aebd707233e2ce263f939996b
Steffen Kampmann <steffen.kampmann@gmail.com>
2019-11-02 16:25:56 +0100

    Init module

---
+++ PSRTools.build.ps1
@@ -0,0 +1,47 @@
+function Get-BuildRoot {
+	if ( $env:APPVEYOR ) {
+		Write-Output $env:APPVEYOR_BUILD_FOLDER
+	} else {
+		Write-Output '.'
+	}
+}
+
+[string] $root = Get-BuildRoot
+[string] $sourcePath = "$root\src"
+[string] $buildPath = "$root\build"
+[string] $docPath = "$root\docs"
+[string] $manifestFilePath = "$sourcePath\PSRTools.psd1"
+[string] $moduleBuildPath = "$buildPath\PSRTools"
+
+task Build PrepareBuildPath, CopyArtefacts
+
+task PrepareBuildPath {
+	New-Item -Path $buildPath -ItemType Directory | Out-Null
+}
+
+task CleanBuildPath {
+	Remove-Item $buildPath -Recurse -ErrorAction 'Continue'
+}
+
+task CopyArtefacts {
+	Copy-Item -Path $sourcePath -Destination $moduleBuildPath -Recurse
+}
+
+task Test {
+    Invoke-Pester $sourcePath
+}
+
+task Publish {
+	Publish-Module -Path $moduleBuildPath -NuGetApiKey $env:nuget_apikey
+}
+
+task Clean CleanBuildPath
+
+task UpdateDocs {
+	Import-Module $manifestFilePath -Force
+	Remove-Item -Path $docPath/*
+	New-MarkdownHelp -Module PSRTools -OutputFolder $docPath
+	# Update-MarkdownHelp -Path $docPath -AlphabeticParamsOrder -Force
+}
+
+task . Clean, Build
