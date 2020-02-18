c564f6fd412ab69c028e3352d5773ba2441ec5bf
Steffen Kampmann <steffen.kampmann@gmail.com>
2020-02-18 09:20:47 +0100

    Fixed dependency to R open in tests

--- src/Test/Set-ScriptPath.Tests.ps1
+++ src/Test/Set-ScriptPath.Tests.ps1
@@ -7,12 +7,13 @@ param (
 
 Describe 'Set-RScript' {
     It 'works with a valid path' {
-        Set-RScriptPath -Path 'C:\Program Files\Microsoft\R Open\R-3.5.2\bin\x64\Rscript.exe'
+        # eg. C:\Program Files\Microsoft\R Open\R-3.5.3\bin\x64 must be in the PATH environment variable
+        Set-RScriptPath -Path ( & where.exe 'Rscript.exe' )
     }
 
     It 'fails with a invalid path' {
         {
-            Set-RScriptPath -Path 'C:\Program Files\Microsoft\R Open\R-3.5.2\bin\x64\R.exe'
+            Set-RScriptPath -Path ( & where.exe 'R.exe' )
         } | Should -Throw
     }
 
